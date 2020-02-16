e76cd5f1a97f0a1df9156fea98f4c0eef48822e5
Meralds-World <grace.chidumaga@gmail.com>
2020-02-16 18:15:47 +0100

    Create date handler payload

---
+++ src/main/java/com/codeWithMerald/fashionBlog/payload/DateHandlerPayload.java
@@ -0,0 +1,26 @@
+package com.codeWithMerald.fashionBlog.payload;
+
+import java.sql.Timestamp;
+
+public class DateHandlerPayload {
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+}
