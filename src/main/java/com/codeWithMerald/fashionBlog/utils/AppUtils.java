dde240f7eafc9aac57e1171e31a3bc2dec8567dc
Meralds-World <grace.chidumaga@gmail.com>
2020-02-16 23:05:29 +0100

    create custom exceptions

--- src/main/java/com/codeWithMerald/fashionBlog/utils/AppUtils.java
+++ src/main/java/com/codeWithMerald/fashionBlog/utils/AppUtils.java
@@ -1,19 +1,20 @@
 package com.codeWithMerald.fashionBlog.utils;
 
+import com.codeWithMerald.fashionBlog.exception.BlogApiException;
 import org.springframework.http.HttpStatus;
 
 public class AppUtils {
     public static void validatePageNumberAndSize(int page, int size) {
         if(page < 0) {
-            throw new BlogapiException(HttpStatus.BAD_REQUEST, "Page number cannot be less than zero.");
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Page number cannot be less than zero.");
         }
 
         if(size < 0) {
-            throw new BlogapiException(HttpStatus.BAD_REQUEST, "Size number cannot be less than zero.");
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Size number cannot be less than zero.");
         }
 
         if(size > AppConstants.MAX_PAGE_SIZE) {
-            throw new BlogapiException(HttpStatus.BAD_REQUEST, "Page size must not be greater than " + AppConstants.MAX_PAGE_SIZE);
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Page size must not be greater than " + AppConstants.MAX_PAGE_SIZE);
         }
     }
 }
